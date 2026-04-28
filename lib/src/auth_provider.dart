a8d39345617ba49c7633ec63810485671d8aa7f1
imdheeraj <dheerajprajapati334@gmail.com>
2026-04-28 20:36:52 +0530

    feat: allow passing custom FirebaseAuth instance

--- lib/src/auth_provider.dart
+++ lib/src/auth_provider.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_phone_auth_handler/src/auth_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -8,15 +9,19 @@ class FirebasePhoneAuthProvider extends StatelessWidget {
   const FirebasePhoneAuthProvider({
     super.key,
     required this.child,
+    this.auth,
   });
 
   /// The child of the widget.
   final Widget child;
 
+  /// The [FirebaseAuth] instance to use.
+  final FirebaseAuth? auth;
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<FirebasePhoneAuthController>(
-      create: (_) => FirebasePhoneAuthController(),
+      create: (_) => FirebasePhoneAuthController(auth: auth),
       child: child,
     );
   }
